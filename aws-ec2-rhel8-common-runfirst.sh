bb491a778076713eda6bac04114501c725a7a2de
Michael Whittle <michael@lifecycle-ps.com>
2020-02-12 10:29:39 +0000

    updated common file

--- aws-ec2-rhel8-common-runfirst.sh
+++ aws-ec2-rhel8-common-runfirst.sh
@@ -40,10 +40,20 @@ cp ~/.bash_profile ~wwwadm/.bash_profile
 chmod 644 ~wwwadm/.bash_profile
 chown wwwadm:wwwadm ~wwwadm/.bash_profile
 
+rm -f ~/.vimrc
+cp ~/aws-ec2-install-scripts/assets/profile/.vimrc ~
+chmod 644 ~/.vimrc
+chown root:root ~/.vimrc
+
 rm -f ~wwwadm/.vimrc
 cp ~/.vimrc ~wwwadm/.vimrc
 chmod 644 ~wwwadm/.vimrc
 chown wwwadm:wwwadm ~wwwadm/.vimrc
+
+rm -f ~/.screenrc
+cp ~/aws-ec2-install-scripts/assets/profile/.screenrc ~
+chmod 644 ~/.screenrc
+chown root:root ~/.screenrc
 
 rm -f ~wwwadm/.screenrc
 cp ~/.screenrc ~wwwadm/.screenrc
